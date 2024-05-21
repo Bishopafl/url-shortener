a0040f10425f43f058149db1dffd475557e9bf1b
Adam Lopez <adamf04@gmail.com>
2024-05-21 00:29:25 -0400

    On redirect, I am incrementing the visit count on the short_url and then redirecting the user to the short_urls associative long_url

---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShortUrl;
+use Illuminate\Http\Request;
+
+class RedirectController extends Controller
+{
+    /**
+     * Redirecting the code that was generated to the url originally in csv
+     * 
+     */
+    public function redirect($code)
+    {
+        $short_url = ShortUrl::where('short_code', $code)->firstOrFail();
+
+        // increment the visit count
+        $short_url->incrementVisitCount();
+
+        return redirect()->to($short_url->long_url);
+    }
+}
